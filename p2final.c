63dc14ee738bb7dc3c7344f396739f1e2d35d776
Tejaswini K <94839306+Tejaswini293@users.noreply.github.com>
2022-02-14 06:15:59 +0000

    p2

---
+++ p2final.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter the number\n");
+  scanf("%d",&a);
+  return a;
+}
+int cmp(int a,int b,int c)
+{
+  int l;
+  if ((a>b)&&(a>c))
+  printf("a is big\n");
+  else if((b>c)&&(b>a))
+  printf("b is big\n");
+  else
+  printf("c is big\n");
+  return 0;
+}
+
+void output(int a,int b,int c,int l)
+{
+  printf("largest number is %d\n",l);
+}
+int main()
+{
+  int a,b,c,l;
+  a=input();
+  b=input();
+  c=input();
+  l=cmp(a,b,c);
+  return 0;
+}
