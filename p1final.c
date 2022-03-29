1e400c3ec10b3c8b23062762f1c0673d73260cbd
Tejaswini K <94839306+Tejaswini293@users.noreply.github.com>
2022-03-29 10:21:57 +0000

    p1 final

--- p1final.c
+++ p1final.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-void input(int *a,int *b)
+void input(int*a, int*b)
 {
-  printf("enter the numbers\n");
-  scanf("%d %d",a,b);
+  printf("enter two values");
+  scanf("%d %d" ,a,b);
 }
-void add(int a,int b,int *sum)
+void add(int a,int b, int* sum)
 {
- *sum=a+b;
+*sum=a+b;
 }
 void output(int a,int b,int sum)
 {
-  printf("sum of %d + %d is %d\n",a,b,sum);
+  printf("sum of %d+%d is %d\n", a,b,sum);
 }
 int main()
 {
